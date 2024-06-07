1dea58f780afe93688ad53da764a254f8d4e644e
MonikySouzaLopes <monikysouzalopes@gmail.com>
2024-06-07 14:59:22 -0400

    Atualizado aula 07.06

--- src/app/componentes/Main.js
+++ src/app/componentes/Main.js
@@ -1,12 +1,62 @@
+'use client'
+import { useEffect, useState } from "react";
 import Image from "next/image"
 import styles from "./main.module.css"
-export default async function Main(){
-    const response = await fetch("https://fakestoreapi.com/products");
-    const data = await response.json();
-    
+
+export default function Main(){
+
+    const [listProduct, setListProduct] = useState([]);
+
+    useEffect( ()=> {
+      const getProduct = async() =>{
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+
+        setListProduct(data);
+      };
+      getProduct();
+    }, []);
+
+    const orderAz = () =>{
+      const newList = [...listProduct].sort( (a, b) =>
+          a.title.localeCompare(b.title)
+      );
+      setListProduct(newList);
+    }
+    const orderZa = () =>{
+      let newList = [...listProduct].sort( (a, b) =>
+          a.title.localeCompare(b.title)
+      );
+      newList = newList.reverse();
+      setListProduct(newList);
+    }
+    const orderPrice = () =>{
+      let newList = [...listProduct].sort( (a, b) =>
+          a.price - b.price
+      );
+      setListProduct(newList);
+    }
+    const orderPriceReverse = () =>{
+      let newList = [...listProduct].sort( (a, b) =>
+          a.price - b.price
+      );
+      newList = newList.reverse();
+      setListProduct(newList);
+    }
+
+
+
       return (
+        <>
+        <div>
+          <button onClick={orderAz}>AZ</button>
+          <button onClick={orderZa}>ZA</button>
+          <button onClick={orderPrice}>Menor preço para o maior</button>
+          <button onClick={orderPriceReverse}>Maior preço para o menor</button>
+        </div>
+
         <main className={styles.main}>
-          {data.map((products)=> 
+          {listProduct.map((products)=> 
           
                 <div className={styles.card} key={products.id}>
                   <center><h3>{products.title}</h3></center>
@@ -20,8 +70,8 @@ export default async function Main(){
               src={products.image}/>
               
                 </div>
-         
          )}
         </main>
+        </>
       );
 }
